4d311664fcbe72eec0cba051af61eaf754b44aad
mrifala29 <mrifala29@gmail.com>
2026-03-06 03:26:26 +0700

    Update code for VPC, success

---
+++ modules/vpc/subnet.tf
@@ -0,0 +1,41 @@
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_a_cidr
+  availability_zone       = var.az_a
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_b_cidr
+  availability_zone       = var.az_b
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_a_cidr
+  availability_zone = var.az_a
+
+  tags = {
+    Name = "private-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_b_cidr
+  availability_zone = var.az_b
+
+  tags = {
+    Name = "private-subnet-b"
+  }
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_a_cidr" {
+  type = string
+}
+
+variable "public_subnet_b_cidr" {
+  type = string
+}
+
+variable "private_subnet_a_cidr" {
+  type = string
+}
+
+variable "private_subnet_b_cidr" {
+  type = string
+}
+
+variable "az_a" {
+  type = string
+}
+
+variable "az_b" {
+  type = string
+}
